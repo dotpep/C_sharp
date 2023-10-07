a9ca2882cf06bcc2d11a8aa765f0f410c0a5f9e3
dotpep <pepelnine@gmail.com>
2023-10-07 21:15:39 +0600

    old code commit 1
    * comment 1
    * comment test 2

---
+++ College_Assignments_NET/HW1_tbook_begin/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HW1_tbook_begin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("begin1: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            double perimeter = 4 * a;
+
+            Console.WriteLine("output: " + perimeter);
+        }
+    }
+}
