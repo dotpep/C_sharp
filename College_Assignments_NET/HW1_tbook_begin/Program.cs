9040faa431b54aaec6927a60a7b8a5c0b63fce6f
dotpep <pepelnine@gmail.com>
2023-10-08 03:03:31 +0600

    final test 1 architecture with method and out parameters

--- College_Assignments_NET/HW1_tbook_begin/Program.cs
+++ College_Assignments_NET/HW1_tbook_begin/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Schema;
 
 namespace HW1_tbook_begin
 {
@@ -23,13 +24,69 @@ namespace HW1_tbook_begin
             perimeter = 2 * (a + b);
         }
 
-        static double Variable()
+        static double Begin4(double a)
+        {
+            double pi = 3.14;
+            double length = pi * a;
+            return length;
+        }
+
+        static void Begin5(double a, double b, double n, out double volume, out double area)
+        {
+            volume = a * b * n;
+            area = 2 * (a * b + a * n + b * n);
+        }
+
+        static void Begin6(double a, out double length, out double area)
+        {
+            double pi = 3.14;
+            length = 2 * pi * a;
+            area = pi * a * a;
+        }
+
+        static double Begin7(double a, double b)
+        {
+            double average = (a + b) / 2;
+            return average;
+        }
+
+        static double Begin8(double a, double b)
+        {
+            double geo_average = Math.Sqrt(a * b);
+            return geo_average;
+        }
+
+        static void Begin9(double a, double b)
+        {
+            double square_a = a * a;
+            double square_b = a * b;
+
+            double sum = square_a + square_b;
+            double difference = square_a - square_b;
+            double content = square_a * square_b;
+            double personal = square_a / square_b;
+
+            Console.WriteLine($"сумма: {sum}, разница: {difference}, произведение: {content}, частное: {personal}");
+        }
+
+        static void Begin10(double a, out double volume, out double area)
+        {
+            volume = a * a * a;
+            area = 6 * a * a;
+        }
+
+        static void Variable()
         {
             Console.Write("n: ");
             double n = Convert.ToDouble(Console.ReadLine());
 
-            double result = Begin2(n);
-            return result;
+            // method choose
+            //double result = Begin4(n);
+            //Console.WriteLine("output: " + result);
+
+            // 1 input 2 output
+            Begin10(n, out double a, out double b);
+            Console.WriteLine($"a: {a}, b: {b}");
         }
 
         static void Variable_2x()
@@ -39,18 +96,33 @@ namespace HW1_tbook_begin
             Console.Write("b: ");
             double n2 = Convert.ToDouble(Console.ReadLine());
 
-            // method change
-            Begin3(n, n2, out double a, out double b);
-            Console.WriteLine($"First: {a}, Second: {b}");
+            // method choose
+            //Begin3(n, n2, out double a, out double b);
+
+            // begin 5
+            //Console.Write("c: ");
+            //double n3 = Convert.ToDouble(Console.ReadLine());
+            //Begin5(n, n2, n3, out double a, out double b);
+
+
+            //Console.WriteLine($"First: {a}, Second: {b}");
+
+            // 2 argument 1 output
+            //Console.WriteLine(Begin8(n, n2));
+
+            // 2 args 4 output
+            Begin9(n, n2);
         }
 
         static void Main(string[] args)
         {
             // for 1 variable input
-            //Console.WriteLine("output: " + Variable());
+            Variable();
+
 
             // call void method
-            Variable_2x();
+            //Variable_2x();
+
         }
     }
 }
