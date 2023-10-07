ffc32c91de4db4c71da9aed21357da69112212d4
dotpep <pepelnine@gmail.com>
2023-10-07 19:04:04 +0600

    module class task 7

---
+++ College_Assignments_NET/HomeWork7_class/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HomeWork3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            Laptop laptop = new Laptop("Asus Tuf", "Intel i7", 1000, 15, 70);
+            laptop.Info();
+            laptop.Sum();
+            
+            Laptop laptop_2 = new Laptop("Apple iMac", "Intel i5", 1700, 10, 40);
+            laptop_2.Info();
+            laptop_2.Sum();
+        }
+    }
+}
