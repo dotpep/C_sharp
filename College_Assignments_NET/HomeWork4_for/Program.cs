efccb4cca90a0c25253ed066b07e3f160fa87977
dotpep <pepelnine@gmail.com>
2023-10-07 19:01:10 +0600

    module for task 4

---
+++ College_Assignments_NET/HomeWork4_for/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HomeWork4_for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1
+            //Console.Write("Price: ");
+            //double price = Convert.ToDouble(Console.ReadLine());
+
+
+            //for (double i = 1; i <= 2; i = i + 0.2)
+            //{
+            //    Console.WriteLine($"kg: {i}, price: {i * price}");
+            //}
+
+            //Task 2
+            //Console.Write("n: ");
+            //double a = Convert.ToDouble(Console.ReadLine());
+
+            //double b = 0;
+
+            //for (int i = 0; i <= a; ++i)
+            //{
+            //    b = (double)Math.Pow(a + i, 2);
+            //    Console.WriteLine(b);
+            //}
+
+
+
+            Console.Write("n: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            int b = 1;
+
+            for (int i = 1; i < 10; i = 2)
+            {
+                
+            }
+
+
+        }
+    }
+}
