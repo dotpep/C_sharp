607ec3fa38f9ae487523e2fa84f63759f05cc91b
dotpep <pepelnine@gmail.com>
2023-10-11 03:18:00 +0600

    array task

---
+++ College_Assignments_NET/HomeWork5_array/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace HomeWork5_array
+{
+    internal class Program
+    {
+        static int[,] array = {
+                {1, 2, 3 },
+                {4, 5, 6 },
+                {7, 8, 9 }
+            };
+
+        static void Main(string[] args)
+        {
+            Array_Diagonal();
+
+        }
+        
+        static int Array_Diagonal()
+        {
+            //Console.WriteLine(array[0, 0]);
+            //Console.WriteLine(array[1, 1]);
+            //Console.WriteLine(array[2, 2]);
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.WriteLine(array[i, i]);
+            }
+
+            return 0;
+        }
+
+        static int Array_Horizontal()
+        {
+            //Console.WriteLine(array[0, 0]);
+            //Console.WriteLine(array[1, 0]);
+            //Console.WriteLine(array[2, 0]);
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.WriteLine(array[i, 0]);
+            }
+
+            return 0;
+        }
+
+
+        static int Array_Vertical()
+        {
+            //Console.WriteLine(array[2, 0]);
+            //Console.WriteLine(array[2, 1]);
+            //Console.WriteLine(array[2, 2]);
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.WriteLine(array[2, i]);
+            }
+
+            return 0;
+        }
+
+    }
+}
---
+++ College_Assignments_NET/HomeWork5_array/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HomeWork5_array")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HomeWork5_array")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bd3834e5-09cd-478c-9f06-678509687370")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
