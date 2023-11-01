49bd8e45d8d02dc4d552ba99117ca38aa0c9a30b
dotpep <pepelnine@gmail.com>
2023-11-02 02:14:24 +0600

    array fun test, implementation stairs task

--- College_Assignments_NET/HomeWork5_array/Program.cs
+++ College_Assignments_NET/HomeWork5_array/Program.cs
@@ -12,10 +12,92 @@ namespace HomeWork5_array
 
         static void Main(string[] args)
         {
-            Array_Vertical();
-
+            Test2();
         }
         
+        static int Test1()
+        {
+            int[,] matrix = {
+                {1, 2, 3},
+                {4, 5, 6}, 
+                {7, 8, 9}
+            };
+
+
+            for (int i = 0; i < matrix.GetLength(0); ++i)
+            {
+                Console.WriteLine(matrix[i, i]);
+            }
+
+            return 0;
+
+        }
+
+        static int Test2()
+        {
+            int[,] array = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8 , 9}
+            };
+
+            int[,] matrix = {
+                {1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10},
+                {11, 12, 13, 14, 15},
+                {16, 17, 18, 19, 20},
+                {21, 22, 23, 24, 25}
+            };
+
+            // array stairs
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j <= i; j++) // j <= i
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+            // symbols stairs
+            int n;
+            Console.Write("\n n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*" + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+            Console.WriteLine();
+
+
+
+            // binary code convert formula 2^1
+            for (int i = 7; i >= 1; --i)
+            {
+                Console.Write($"2^{i}" + "\t ");
+            }
+
+            Console.WriteLine(" ");
+            for (int i = 7; i >= 1; --i)
+            {
+                Console.Write(Math.Pow(2, i) + "\t ");
+            }
+            Console.WriteLine();
+
+            return 0;
+        }
+
+
         static int Array_Diagonal()
         {
             //Console.WriteLine(array[0, 0]);
